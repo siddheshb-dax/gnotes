8d952ad418e80280dbd3b0f99cefe46eb3557aa7
siddhesh badnapurkar <Siddhesh.Badnapurkar@data-axle.com>
2026-07-23 05:00:11 +0530

    [chore]: register notes with admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Note
+
+'''
+# Shorter Way:
+admin.site.register(Note)
+'''
+
+'''Another way to register'''
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    pass
