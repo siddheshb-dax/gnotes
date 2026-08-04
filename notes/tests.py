2d6fb3c0d2b4f875d00561bf1db644d798a1a43e
siddheshbadnapurkar <Siddhesh.Badnapurkar@data-axle.com>
2026-08-04 12:57:52 +0530

    [testing]: add test for creating a user

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import json
+import pytest 
+from django.contrib.auth import get_user_model
+
+@pytest.mark.django_db
+def test_create_user_mutation_creates_user(client):
+    mutation = """
+    mutation CreateUser($username: String!, $password: String!) {
+        createUser(username: $username, password: $password) {
+            user {
+                id
+                username
+            }
+        }
+    }
+    """
+
+    variables = {
+        "username": "test_username",
+        "password": "Test@123"
+    }
+
+    response = client.post(
+        "/graphql/",
+        data = json.dumps({"query": mutation, "variables": variables}),
+        content_type = "application/json"
+    )
+
+    assert response.status_code == 200
+    payload = response.json()
+
+    assert "errors" not in payload
+    user = payload["data"]["createUser"]["user"]
+
+    assert user["username"] == "test_username"
+    assert user["id"] is not None
+
+    db_user = get_user_model().objects.get(username="test_username")
+    assert db_user.check_password("Test@123")
