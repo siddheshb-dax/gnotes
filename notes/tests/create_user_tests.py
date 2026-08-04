60d24ddd4edb1cffaf239d7e5047e0f80b7ff61e
siddheshbadnapurkar <Siddhesh.Badnapurkar@data-axle.com>
2026-08-04 13:35:18 +0530

    [chore/testing]: add some insights

--- notes/tests/create_user_tests.py
+++ notes/tests/create_user_tests.py
@@ -8,6 +8,8 @@ from django.contrib.auth import get_user_model
 
 @pytest.mark.django_db
 def test_create_user_mutation_creates_user(client):
+
+    # Define the mutation for creating a user
     mutation = """
     mutation CreateUser($username: String!, $password: String!) {
         createUser(username: $username, password: $password) {
@@ -19,27 +21,44 @@ def test_create_user_mutation_creates_user(client):
     }
     """
 
+    # Define the values to pass 
     variables = {
         "username": "test_username",
         "password": "Test@123"
     }
 
+    # Send the request passing the values and get the resposnse
     response = client.post(
         "/graphql/",
         data = json.dumps({"query": mutation, "variables": variables}),
         content_type = "application/json"
     )
 
+    # Assert successful request
     assert response.status_code == 200
+
+    # Extract the payload from the response 
     payload = response.json()
 
+    # Assert no errors in the response
     assert "errors" not in payload
+
+    # Fetch the user object from the executed mutation
     user = payload["data"]["createUser"]["user"]
 
+    # Assert correct username
     assert user["username"] == "test_username"
+
+    # assert that the ID of the created user is not none
     assert user["id"] is not None
 
+    # Fetch the user from the Test DB
     db_user = get_user_model().objects.get(username="test_username")
+
+    '''
+        check_password fetches the same salt and hashing algorithm
+        and matches the hashed password for the user
+    '''
     assert db_user.check_password("Test@123")
 
 @pytest.mark.django_db
