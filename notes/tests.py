0cf1ba0cb6ced87dfb1c3ccf5a1065a0c2951c00
siddheshbadnapurkar <Siddhesh.Badnapurkar@data-axle.com>
2026-08-04 13:12:54 +0530

    [testing]: test create user without password

--- notes/tests.py
+++ notes/tests.py
@@ -41,3 +41,40 @@ def test_create_user_mutation_creates_user(client):
 
     db_user = get_user_model().objects.get(username="test_username")
     assert db_user.check_password("Test@123")
+
+@pytest.mark.django_db
+def test_create_user_without_password_fails(client):
+    mutation = """
+    mutation CreateUserWithoutPassword($username: String!, $pw: String!) {
+        createUser(username: $username, password: $pw) {
+            user {
+                username
+            }
+        }
+    }
+    """
+
+    variables = {
+        "username": "test_username"
+        # pw missing
+    }
+
+    response = client.post(
+        "/graphql/",
+        data = json.dumps({
+            "query": mutation,
+            "variables": variables
+        }),
+        content_type = "application/json"
+    )
+
+    # changed from 200 as graphene resolves the request strictly based on internal
+    # schema, we expect a 400 error code here. 
+    assert response.status_code == 400 
+
+    payload = response.json()
+
+    assert "errors" in payload
+
+    assert not get_user_model().objects.filter(username="test_username").exists()
+
